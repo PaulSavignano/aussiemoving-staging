60030c42c0d9518860b896c7e3f11b59fae0d365
Paul Savignano <Paul.Savignano@gmail.com>
2017-05-16 07:07:10 -0700

    Refactor theme and add cors to s3 bucket

--- client/src/theme/containers/AdminThemePage.js
+++ client/src/theme/containers/AdminThemePage.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import AdminThemeAdd from '../components/AdminThemeAdd'
 import AdminTheme from '../components/AdminTheme'
 import AdminFavicon from '../components/AdminFavicon'
 
